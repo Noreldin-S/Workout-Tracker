46170cae5c01b7a975d9126323d9dcb2cdd227d4
Noreldin Saad <s.noreldin@hotmail.com>
2021-05-03 21:02:36 -0700

    final

--- server.js
+++ server.js
@@ -1,34 +1,62 @@
   
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path")
-const cors = require("cors")
-const logger = require("morgan")
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const path = require("path")
+// const cors = require("cors")
+// const logger = require("morgan")
 
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
+
+// const app = express();
+
+// app.use(logger("dev"))
+
+// require('dotenv').config();
+
+// app.use(cors());
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// // app.use(express.static("public"));
+// app.use(express.static(__dirname + '/public'));
+
+// require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+// const uri = process.env.MONGO_URI;
+// mongoose.connect(uri, { useNewUrlParser: true });
+// // const connection = mongoose.connection;
+// // connection.once('open', () => {
+// //   console.log('MongoDB database connection established successfully');
+// // });
+// //mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParse: true});
+
+// app.listen(PORT, () =>{
+//     console.log("Listening on PORT " + PORT)
+// })
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"))
+app.use(logger("dev"));
 
-require('dotenv').config();
-
-app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
-app.use(express.static(__dirname + '/public'));
 
-require("./routes/htmlRoutes")(app)
-require("./routes/apiRoutes")(app);
-const uri = process.env.MONGO_URI;
-// mongoose.connect(uri, { useNewUrlParser: true });
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//   console.log('MongoDB database connection established successfully');
-// });
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParse: true});
+app.use(express.static("public"));
 
-app.listen(PORT, () =>{
-    console.log("Listening on PORT " + PORT)
-})
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
